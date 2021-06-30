30a757ca797ac4649fb295e97b09771dad4b47b7
dima <konoval6991@gmail.com>
2021-06-30 18:16:12 +0300

    Added functionality to create card for every  doc of the collection 'books', and than display these cards in browser

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ const db = firebase.firestore();
 const library = [];
 
 const addBookBtn = document.querySelector('.addBookBtn');
-const booksList = document.querySelector('.booksList');
+const cardsList = document.querySelector('.cardsList');
 
 // Generate and return unique id
 
@@ -57,8 +57,49 @@ function getBooks() {
 			querySnapshot.forEach((doc) => {
 				const id = doc.id;
 				library.push({ id, ...doc.data() });
-				console.log(library);
+				
 			});
+		}).then(() =>{
+		const fragment = document.createDocumentFragment();
+		
+		const cards = library.forEach((book) => {
+		  fragment.appendChild(createCard(book));
+		});
+		cardsList.append(fragment);
 		});
 }
+
+function createCard(props){
+  const {title, author, isRead, pages, id} = props;
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.setAttribute('id', id);
+  const pTitle = document.createElement('p');
+  pTitle.textContent = title;
+  
+  const pAuthor = document.createElement('p');
+  pAuthor.textContent = author;
+ 
+ const pPages = document.createElement('p');
+ pPages.textContent = `${pages} pages`;
+ 
+ const pIsRead = document.createElement('p');
+ pIsRead.textContent = 'Have already read?';
+ 
+ const lSwitch = document.createElement('label');
+ lSwitch.classList.add('switch');
+ const input = document.createElement('input');
+ 
+ input.checked = isRead;
+ input.type = 'checkbox';
+ const sSlider = document.createElement('span');
+ sSlider.classList.add('slider');
+ sSlider.classList.add('round');
+ lSwitch.append(input, sSlider);
+ card.append(pTitle, pAuthor,pPages, pIsRead, lSwitch);
+ return card;
+}
 getBooks();
+
+/*console.log(createCard({title:'Martin Eden', author: 'Jack London', pages: 235, isRead: false}));
+*/
